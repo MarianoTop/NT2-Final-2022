e0f8e631664f048be7ba2efe8f6097b3f851e1e5
Mariano <marianotoppino@gmail.com>
2022-12-13 20:41:03 -0300

    Se realizan modificaiciones pero no se termina el opcional

--- src/componentes/Conversor.vue
+++ src/componentes/Conversor.vue
@@ -5,7 +5,11 @@
 
     <span>Ingrese monto $</span>  <input type="text" v-model="pesos"> <br>
     <br>
-    <span>Valor del dolar en $</span>  <input type="text" v-model="valorDolar"> <br><br>
+    <span>Valor del dolar en $</span>  <input type="text" v-model="valorDolar">  
+    <span> Actualizacion </span> <input type="checkbox" v-model="actualizacionAutomatica">
+    
+    <br><br>
+    
     <span>Valor convertido USD </span> {{pesos/valorDolar}} <br> <br>
 
          <br>
@@ -28,14 +32,41 @@
     data () {
       return {
         pesos:0,
-        valorDolar:300
+        valorDolar:300,
+        urlDolarPrecio: 'https://www.dolarsi.com/api/api.php?type=valoresprincipales',
+        actualizacionAutomatica:false
 
       }
     },
     methods: {
 
+      async getCotizacion() {
+        try {
+          let response = await this.axios(this.urlDolarPrecio)
+          let respuesta = response.data
+
+         
+
+          let cotizacionBlue=respuesta.find( function(x){
+            
+            return x.casa.nombre=='Dolar Blue'
+          })
+
+          let valorBlueApi= cotizacionBlue.casa.compra
+          
+          return valorBlueApi
+         
+
+        }
+        catch(error) {
+           console.error('Se produjo un error al obtener la cotizacion', error)
+        }
+      },
+       
     },
     computed: {
+
+      
 
     }
 }
