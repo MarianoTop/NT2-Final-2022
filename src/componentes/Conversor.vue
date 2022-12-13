8451d9876ea4b46dc8cbc17e8c7601cf33f90f45
Mariano <marianotoppino@gmail.com>
2022-12-13 19:49:59 -0300

    primer commit

---
+++ src/componentes/Conversor.vue
@@ -0,0 +1,50 @@
+<template >
+
+  <section class="src-componentes-conversor">
+    <h1>Conversor a dólares</h1>
+
+    <span>Ingrese monto $</span>  <input type="text" v-model="pesos"> <br>
+    <br>
+    <span>Valor del dolar en $</span>  <input type="text" v-model="valorDolar"> <br><br>
+    <span>Valor convertido USD </span> {{pesos/valorDolar}} <br> <br>
+
+         <br>
+      <h4> Respuestas a las preguntas </h4>
+
+      <p> respuestas : 1  , 2 ,3 ,4 ,5 
+
+  </section>
+
+</template>
+
+<script>
+
+  export default  {
+    name: 'src-componentes-conversor',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+        pesos:0,
+        valorDolar:300
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-componentes-conversor {
+
+  }
+</style>
